a17528119eeed94434b78251b5f76f7e9719a667
Aylin DeBruyne <adebruyne88@gmail.com>
2018-11-05 11:08:44 -0500

    selects all from todos

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+// const pgPromise = require('pg-promise');
+
 const pgp = require("pg-promise")();
+
+const db = pgp({
+  host: "localhost",
+  port: 5432,
+  database: "node-todo-app-db-dir"
+});
+
+db.any("select * from todos").then(results => {
+  console.log(results);
+});
